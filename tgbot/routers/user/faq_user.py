7dbd04f78653f9a2294a3deb0589acdb5182bda5
hdhejshhdhej <doneru703@gmail.com>
2026-07-25 22:16:16 +0300

    Update faq_user.py

--- tgbot/routers/user/faq_user.py
+++ tgbot/routers/user/faq_user.py
@@ -1,4 +1,3 @@
-from aiogram.filters import Text
 from aiogram.types import Message, CallbackQuery
 
 from tgbot.keyboards.admin.faq_inline import see_faq_user, back_faq_info
@@ -12,13 +11,13 @@ faq_user_router = Router()
 
 
 
-@faq_user_router.message(Text(text="ℹ️ Инфо"))
+@faq_user_router.message(F.text == "ℹ️ Инфо")
 async def faq_menu(message: Message, state: FSM):
     await state.clear()
     all_faq = get_faqs(is_show=True)
     await message.answer('Пожалуйста, выберете интересующий вас вопрос', reply_markup=info_link(all_faq))
 
-@faq_user_router.callback_query(Text(text="faq_info"))
+@faq_user_router.callback_query(F.data == "faq_info")
 async def faq_menu(call: CallbackQuery, state: FSM):
     await state.clear()
     all_faq = get_faqs(is_show=True)
@@ -31,4 +30,3 @@ async def faq_menu(call: CallbackQuery, state: FSM):
     await state.clear()
     all_faq = get_faq(id=call.data.split(';')[1])
     await call.message.edit_text(text=all_faq['description'], reply_markup=back_faq_info(),disable_web_page_preview=False)
-
