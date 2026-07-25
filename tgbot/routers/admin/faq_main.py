c0a12dd62889705e24b90b208f8f02d0fc5d1ac5
hdhejshhdhej <doneru703@gmail.com>
2026-07-25 21:02:38 +0300

    Update faq_main.py

--- tgbot/routers/admin/faq_main.py
+++ tgbot/routers/admin/faq_main.py
@@ -1,5 +1,4 @@
 from aiogram import Router, Bot, F
-from aiogram.filters import Text
 from aiogram.types import Message, CallbackQuery
 
 from tgbot.keyboards.admin.faq_inline import create_faq, edit_faq
@@ -11,16 +10,17 @@ from tgbot.utils.states import NewFaq
 admin_faq_router = Router()
 
 
-@admin_faq_router.message(Text(text="FAQ"))
-async def faq_admin_main(message:Message, state:FSM, ):
+@admin_faq_router.message(F.text == "FAQ")  # <--- ИСПРАВЛЕНО
+async def faq_admin_main(message: Message, state: FSM, ):
     await state.clear()
     all_faq = get_all_faq()
 
     text = "Выберете раздел для редактирования либо создайте новый"
     await message.answer(text=text, reply_markup=create_faq(all_faq))
 
-@admin_faq_router.callback_query(Text(text="create_faq"))
-async def new_faq(call:CallbackQuery, state:FSM):
+
+@admin_faq_router.callback_query(F.data == "create_faq")  # <--- ИСПРАВЛЕНО
+async def new_faq(call: CallbackQuery, state: FSM):
     await state.clear()
     text = "Отправьте название кнопки"
     await call.message.answer(text=text, reply_markup=close_this)
@@ -29,42 +29,45 @@ async def new_faq(call:CallbackQuery, state:FSM):
 
 
 @admin_faq_router.message(NewFaq.button, F.text)
-async def button_faq(message:Message, state:FSM):
+async def button_faq(message: Message, state: FSM):
     await state.update_data(button=message.text)
-    text = "Отправтьте описание"
+    text = "Отправьте описание"
     await message.answer(text=text)
     await state.set_state(NewFaq.description)
 
+
 @admin_faq_router.message(NewFaq.button)
-async def not_text_button_faq(message:Message, state:FSM):
+async def not_text_button_faq(message: Message, state: FSM):
     text = "Нужно отправить текст"
     await message.answer(text=text)
 
+
 @admin_faq_router.message(NewFaq.description, F.text)
-async def write_faq_handler(message:Message, state:FSM):
+async def write_faq_handler(message: Message, state: FSM):
     description = message.html_text
     data = await state.get_data()
     text_button = data['button']
-    write_faq(name_button=text_button, description=description )
+    write_faq(name_button=text_button, description=description)
     all_faq = get_all_faq()
     text = "Выберете раздел для редактирования либо создайте новый"
     await message.answer(text=text, reply_markup=create_faq(all_faq))
 
-@admin_faq_router.message(NewFaq.description, F.text)
-async def not_write_faq(message:Message,):
+
+@admin_faq_router.message(NewFaq.description)
+async def not_write_faq(message: Message, ):
     text = "Нужно отправить текст"
     await message.answer(text=text)
 
 
 @admin_faq_router.callback_query(F.data.startswith("edit_faq"))
-async def edit_faq_handler(call:CallbackQuery, state:FSM):
+async def edit_faq_handler(call: CallbackQuery, state: FSM):
     await state.clear()
     line = int(call.data.split(';')[1])
     info = get_faq(id=line)
     is_show = info['is_show']
     text_ru = f"{info['description']}\n\n"
     text = text_ru
-    await call.message.edit_text(text=text, reply_markup=edit_faq(line,is_show))
+    await call.message.edit_text(text=text, reply_markup=edit_faq(line, is_show))
 
 
 
